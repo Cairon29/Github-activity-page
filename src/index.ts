930f6b2bba78fffea91faa711d4a46fdc2d8388e
Cairon29 <josewjjunco@gmail.com>
2025-11-17 21:05:24 -0500

    fix: basic TS + express sintaxis errors resolved

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { type Request, type Response }  from 'express';
-import { json } from 'stream/consumers';
 
 const app = express()
 
@@ -8,30 +7,23 @@ const PORT = 5555
 app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
-    return "Here a main endpoint for the github page"
+
+    res.json({ response: "here the response" });
 })
 
-/*
-    const id = req.params
-    let { id_2 } = req.query;
-    const { email, password } = req.body;
-*/
-
 app.get("/user/:name", async (req: Request, res: Response) => {
-    res.send({"Github activity app currently starting": 1})
     const { name } = req.params
 
     const raw = await fetch(`https://api.github.com/users/${name}`)
     const data = await raw.json()
     console.log(data)
-    return data
+    res.json(data)
 })
 
 app.get("/repos", async (req: Request, res: Response)  => {
     const raw = await fetch('https://api.github.com/users/Cairon29/repos')
     const data = await raw.json()
-
-    return data
+    res.json(data)
 })
 
 app.listen(PORT, () => {
